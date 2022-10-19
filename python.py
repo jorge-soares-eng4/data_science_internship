679a984225dc819987e0e23bbebedd28410b29ad
Gustavo Niero <gubniero13@gmail.com>
2022-10-19 18:30:46 -0300

    Segundo Plot, adição de colorbar

--- python.py
+++ python.py
@@ -1,8 +1,11 @@
 #imports
+from tkinter.ttk import Style
+from turtle import color, title
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-#read
+import matplotlib as mpl
+from matplotlib.colors import Normalize#read
 df = pd.read_csv('data.csv', sep=";")
 
 corte = df.corte.value_counts()
@@ -11,18 +14,27 @@ print(corte)
 sns.set_theme(style='darkgrid')
 
 #plot
+ax = plt.subplots()
+norm = plt.Normalize(1.74, 1.84)
+sm = plt.cm.ScalarMappable(norm=norm)
+sm.set_array([])
 
+ax = sns.scatterplot(x='altura',y='altura',data=df,)
 
 sns.scatterplot(
     x=df.quilates,
     y=df.preço,
     style=df.corte,
-    size=df.plato, sizes=(30, 400),
+    size=df.plato, sizes=(0, 400),
     hue=df.altura,
+    ax=ax,
 )
+ax.get_legend().remove()
+ax.figure.colorbar(sm)
+ax.set_title("altura")
 plt.xscale("log")
 plt.title('Preço por Quilates')
 plt.xlabel("Quilates(ct)")
 plt.ylabel("Preço(k)")
-plt.legend(loc=4, title="deu certo")
+plt.legend(loc=4)
 plt.show()
